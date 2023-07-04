e95104248f3980d841af42c547c8e0f2209ea308
sanduntharaka <sanduntg@yahoo.com>
2023-07-04 08:40:05 +0530

    change p user

--- core/settings.py
+++ core/settings.py
@@ -166,8 +166,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bixtonlightingdb',
-        'USER': 'bixtonlightinguser',
-        'PASSWORD': 'AVNS_qZvei7W6FX92Jr_4qx4',
+        'USER': 'doadmin',
+        'PASSWORD': 'AVNS_GlClCnbEOaZm-02AQFW',
         'HOST': 'db-postgresql-blr1-42114-do-user-13856971-0.b.db.ondigitalocean.com',
         'PORT': '25060',
     }
