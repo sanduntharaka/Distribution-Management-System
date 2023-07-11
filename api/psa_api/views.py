71a693fdec3f10ff02ee0c1ef4d9c98d83e9f408
sanduntharaka <sanduntg@yahoo.com>
2023-07-11 09:46:46 +0530

    chnage only related psa show

--- api/psa_api/views.py
+++ api/psa_api/views.py
@@ -1,3 +1,8 @@
+from manager_distributor.models import ManagerDistributor
+from distrubutor_salesref.models import SalesRefDistributor
+from userdetails.models import UserDetails
+from executive_distributor.models import ExecutiveDistributor
+from exceutive_manager.models import ExecutiveManager
 from rest_framework import filters
 from rest_framework import status
 from rest_framework.response import Response
@@ -14,15 +19,140 @@ class CreatePSA(generics.CreateAPIView):
 
 class AllCreatedPsa(generics.ListAPIView):
     serializer_class = serializers.GetAllPSASerializer
-    queryset = PrimarySalesArea.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user.id
+        users = []
+        if (self.request.user.is_company):
+
+            return get_list_or_404(PrimarySalesArea)
+
+        if (self.request.user.is_excecutive):
+            manager = ExecutiveManager.objects.get(
+                executive__user=user).manager.id
+            users.append(manager)
+            distributors = ExecutiveDistributor.objects.filter(
+                executive__user=user).values_list('distributor', flat=True)
+            distributors_ids = UserDetails.objects.filter(
+                id__in=distributors).values_list('user', flat=True)
+            users.extend(distributors_ids)
+            salesrefs_ids = SalesRefDistributor.objects.filter(
+                distributor__user__in=distributors_ids).values_list('sales_ref', flat=True)
+            salesref_users_ids = UserDetails.objects.filter(
+                id__in=salesrefs_ids).values_list('user', flat=True)
+
+            users.extend(salesref_users_ids)
+        if (self.request.user.is_manager):
+            users.append(user)
+
+            distributors = ManagerDistributor.objects.filter(
+                manager__user=user).values_list('distributor', flat=True)
+            distributors_ids = UserDetails.objects.filter(
+                id__in=distributors).values_list('user', flat=True)
+            users.extend(distributors_ids)
+            salesrefs_ids = SalesRefDistributor.objects.filter(
+                distributor__user__in=distributors_ids).values_list('sales_ref', flat=True)
+            salesref_users_ids = UserDetails.objects.filter(
+                id__in=salesrefs_ids).values_list('user', flat=True)
+
+            users.extend(salesref_users_ids)
+        if (self.request.user.is_distributor):
+            users.append(user)
+            manager = ManagerDistributor.objects.get(
+                distributor__user=user).manager.user.id
+            users.append(manager)
+            salesrefs = SalesRefDistributor.objects.filter(
+                distributor__user=user).values_list('sales_ref', flat=True)
+            salesref_users_ids = UserDetails.objects.filter(
+                id__in=salesrefs).values_list('user', flat=True)
+            users.extend(salesref_users_ids)
+
+        if (self.request.user.is_salesref):
+
+            distributor = SalesRefDistributor.objects.get(
+                sales_ref__user=user).distributor.user.id
+            users.append(distributor)
+            manager = ManagerDistributor.objects.get(
+                distributor__user=distributor).manager.user.id
+            users.append(manager)
+
+            salesrefs = SalesRefDistributor.objects.filter(
+                distributor__user=distributor).values_list('sales_ref', flat=True)
+            salesref_users_ids = UserDetails.objects.filter(
+                id__in=salesrefs).values_list('user', flat=True)
+            users.extend(salesref_users_ids)
+
+        return get_list_or_404(PrimarySalesArea, created_by__in=users)
 
 
 class GetAllSearch(generics.ListAPIView):
     serializer_class = serializers.GetAllPSASerializer
-    queryset = PrimarySalesArea.objects.all()
-
     filter_backends = [filters.SearchFilter]
     search_fields = ('area_name',)
+
+    def get_queryset(self):
+        user = self.request.user.id
+        users = []
+        if (self.request.user.is_company):
+
+            return get_list_or_404(PrimarySalesArea)
+
+        if (self.request.user.is_excecutive):
+            manager = ExecutiveManager.objects.get(
+                executive__user=user).manager.id
+            users.append(manager)
+            distributors = ExecutiveDistributor.objects.filter(
+                executive__user=user).values_list('distributor', flat=True)
+            distributors_ids = UserDetails.objects.filter(
+                id__in=distributors).values_list('user', flat=True)
+            users.extend(distributors_ids)
+            salesrefs_ids = SalesRefDistributor.objects.filter(
+                distributor__user__in=distributors_ids).values_list('sales_ref', flat=True)
+            salesref_users_ids = UserDetails.objects.filter(
+                id__in=salesrefs_ids).values_list('user', flat=True)
+
+            users.extend(salesref_users_ids)
+        if (self.request.user.is_manager):
+            users.append(user)
+
+            distributors = ManagerDistributor.objects.filter(
+                manager__user=user).values_list('distributor', flat=True)
+            distributors_ids = UserDetails.objects.filter(
+                id__in=distributors).values_list('user', flat=True)
+            users.extend(distributors_ids)
+            salesrefs_ids = SalesRefDistributor.objects.filter(
+                distributor__user__in=distributors_ids).values_list('sales_ref', flat=True)
+            salesref_users_ids = UserDetails.objects.filter(
+                id__in=salesrefs_ids).values_list('user', flat=True)
+
+            users.extend(salesref_users_ids)
+        if (self.request.user.is_distributor):
+            users.append(user)
+            manager = ManagerDistributor.objects.get(
+                distributor__user=user).manager.user.id
+            users.append(manager)
+            salesrefs = SalesRefDistributor.objects.filter(
+                distributor__user=user).values_list('sales_ref', flat=True)
+            salesref_users_ids = UserDetails.objects.filter(
+                id__in=salesrefs).values_list('user', flat=True)
+            users.extend(salesref_users_ids)
+
+        if (self.request.user.is_salesref):
+
+            distributor = SalesRefDistributor.objects.get(
+                sales_ref__user=user).distributor.user.id
+            users.append(distributor)
+            manager = ManagerDistributor.objects.get(
+                distributor__user=distributor).manager.user.id
+            users.append(manager)
+
+            salesrefs = SalesRefDistributor.objects.filter(
+                distributor__user=distributor).values_list('sales_ref', flat=True)
+            salesref_users_ids = UserDetails.objects.filter(
+                id__in=salesrefs).values_list('user', flat=True)
+            users.extend(salesref_users_ids)
+
+        return PrimarySalesArea.objects.filter(created_by__in=users)
 
 
 class EditPsa(generics.UpdateAPIView):
