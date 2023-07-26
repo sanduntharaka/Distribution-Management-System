046415e901be8598baa7382e2fd3b2dadb98132f
sanduntharaka <sanduntg@yahoo.com>
2023-07-27 00:24:38 +0530

    changed cheque realize

--- backgrountasks/cheque_realize.py
+++ backgrountasks/cheque_realize.py
@@ -10,18 +10,22 @@ def realizeAllCheques():
     yesterday_cheques = ChequeDetails.objects.filter(
         date=yesterday, status='pending')
     for cheque in yesterday_cheques:
-        cheque.status = 'cleared'
-        cheque.save()
-        pay_details = cheque.payment_details
-        pay_details.is_completed = True
-        pay_details.save()
+        try:
+            cheque.status = 'cleared'
+            cheque.save()
+            pay_details = cheque.payment_details
+            pay_details.is_completed = True
+            pay_details.save()
 
-        bill = PaymentDetails.objects.get(id=cheque.payment_details.id).bill
-        if bill.get_payed() == bill.total:
-            bill.is_settiled = True
-            bill.save()
-
-    f = open("./logs/cheque_realize/logs.txt", "a")
-    f.write(
-        f"{current_date} confirmed cheque date {yesterday}  total {len(yesterday_cheques)}\n")
-    f.close()
+            bill = PaymentDetails.objects.get(
+                id=cheque.payment_details.id).bill
+            if bill.get_payed() == bill.total:
+                bill.is_settiled = True
+                bill.save()
+            with open("./logs/cheque_realize/logs.txt", "a") as f:
+                f.write(
+                    f"Success {current_date} confiremed cheque date {yesterday } cheque {cheque.cheque_number} \n")
+        except Exception as e:
+            with open("./logs/cheque_realize/logs.txt", "a") as f:
+                f.write(
+                    f"Error {current_date} cheque  {cheque.cheque_number} error {e} \n")
