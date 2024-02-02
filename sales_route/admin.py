090db4c928ac25ba953c21682afccb2aaa1692b3
sanduntharaka <sanduntg@yahoo.com>
2024-02-02 13:29:18 +0530

    route admin setup

--- sales_route/admin.py
+++ sales_route/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import SalesRoute, DailyStatus
-admin.site.register(SalesRoute)
+
 admin.site.register(DailyStatus)
+
+
+class SalesRoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'salesref',
+                    'date', 'is_approved', 'created_at', 'updated_at', 'approved_by')
+    list_display_links = ('id', 'salesref')
+    list_filter = ('date', 'is_approved')
+    search_fields = ('date',)
+    list_per_page = 25
+
+
+admin.site.register(SalesRoute, SalesRoteAdmin)
